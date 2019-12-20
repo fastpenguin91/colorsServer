7911012c2e40182bd98feebc01f50cd020e6014a
John Curry <fastpenguin91@gmail.com>
2019-12-20 14:44:01 -0800

    formatted code

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
-const { prisma } = require('./generated/prisma-client')
-const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require("./generated/prisma-client");
+const { GraphQLServer } = require("graphql-yoga");
 
 const resolvers = {
   Query: {
     colors: (parent, args, context, info) => {
-        const colors = context.prisma.colors({
-          skip: args.skip,
-          first: args.first
-        })
-        return colors 
-      },
-    color: (root, args, context, info) => {
-        return context.prisma.color()
+      const colors = context.prisma.colors({
+        skip: args.skip,
+        first: args.first
+      });
+      return colors;
     },
+    color: (root, args, context, info) => {
+      return context.prisma.color();
+    }
   },
 
   Mutation: {
-      setColor: (root, args, context) => {
-          return context.prisma.createColor({
-              color_code: args.color_code,
-          })
-      },
+    setColor: (root, args, context) => {
+      return context.prisma.createColor({
+        color_code: args.color_code
+      });
+    }
   },
 
   Color: {
-      id: (parent) => parent.id,
-      color_code: (parent) => parent.color_code,
+    id: parent => parent.id,
+    color_code: parent => parent.color_code
   }
-
-}
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
-  context: { prisma },
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+  context: { prisma }
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
