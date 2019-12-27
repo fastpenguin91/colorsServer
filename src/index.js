777100b48eb87ac7a8803c4cb73a0a9bdcfc84e8
John Curry <fastpenguin91@gmail.com>
2019-12-27 15:22:19 -0800

    removed unnecessary resolvers

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
 const { prisma } = require("./generated/prisma-client");
 const { GraphQLServer } = require("graphql-yoga");
-const { find } = require('lodash');
+//const { find } = require('lodash');
 
 
-const resolvers = {
-  Query: {
-    colors: (parent, args, context, info) => {
-      const colors = context.prisma.colors({
-        skip: args.skip,
-        first: args.first
-      });
-      return colors;
-    },
-    color: (root, args, context, info) => {
-      return context.prisma.color();
-    }
-  },
+//const resolvers = {
+  //Query: {
+    //colors: (parent, args, context, info) => {
+     // const colors = context.prisma.colors({
+       // skip: args.skip,
+       // first: args.first
+      //});
+      //return colors;
+    //},
+    //color: (root, args, context, info) => {
+      //return context.prisma.color();
+    //}
+  //},
 
-  Mutation: {
-    setColor: (root, args, context) => {
-      return context.prisma.createColor({
-        color_code: args.color_code
-      });
-    }
-  },
+  //Mutation: {
+    //setColor: (root, args, context) => {
+      //return context.prisma.createColor({
+        //color_code: args.color_code
+      //});
+    //}
+  //},
 
-  Color: {
-    id: parent => parent.id,
-    color_code: parent => parent.color_code
-  }
-};
+  //Color: {
+    //id: parent => parent.id,
+    //color_code: parent => parent.color_code
+  //}
+//};
 
 const server = new GraphQLServer({
-  typeDefs: "./src/schema.graphql",
-  resolvers,
+  //typeDefs: "./src/schema.graphql",
+  //resolvers,
+  schema: "../prisma/datamodel.prisma,"
   context: { prisma }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
