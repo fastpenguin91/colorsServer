a39299dfedba8e660f1a322aa8de83c84a69c00f
John Curry <fastpenguin91@gmail.com>
2019-12-31 14:43:40 -0800

    added test info query and resolvers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const { prisma } = require("./generated/prisma-client");
 const { GraphQLServer } = require("graphql-yoga");
 
+// I think the object here in graphqlServer and graphQLOptions are supposed to do the same thing
+// so one of them is unnecessary?
 const server = new GraphQLServer({
   schema: "../prisma/datamodel.prisma",
+  typeDefs: './schema.graphql',
+  resolvers: resolvers,
   context: { prisma }
 });
 
+const resolvers = {
+  Query: {
+    info: () => `This is a test query defined in resolvers`
+  }
+}
+
 const graphQLOptions = {
   endpoint: 'https://eu1.prisma.sh/fastpenguin91-c6edf8/color-swatches-server/dev',
-  typeDefs: './schema.graphql'
+  typeDefs: './schema.graphql',
+  resolvers: resolvers
   //endpoint: 'https://us1.prisma.sh/fastpenguin91-c6edf8/finaltest/dev'
 }
 
